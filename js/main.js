7a622d89ab9eab9fa666cefd36545752d27af343
Anupam-dagar <dagaranupam@gmail.com>
2017-04-18 22:37:55 +0530

    Added picture and personal data with 2 experiences.Changed text for intro automated text.

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Aniket Dagar", "I'm an IIT Kharagpur ECE undergraduate", "I like to code", "I am a Data Scientist"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
